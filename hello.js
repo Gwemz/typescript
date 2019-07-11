2104ffd542dbb107e2865d4cda2a2c3774a24d0e
guowentao <gwem.win@gmail.com>
2019-07-11 15:14:53 +0800

    'ts'

--- hello.js
+++ hello.js
@@ -1,5 +1,13 @@
-var hello = function (str) {
-    console.log('typescript');
-    return "hello," + str;
-};
-hello('daguo');
+// const hello = str => {
+//     console.log('typescript');
+//     console.log('this is a function start');
+//     return `hello,${str}`;
+// }
+// hello('daguo');
+var decLiteral = 6; //数字类型
+var name1 = 'bob'; //字符串类型
+var sentence = "hello,my name is " + name1; //字符串模板
+var list0 = [1, 2, 3, 4]; //[]形式定义数组
+var list1 = ['12', '12', '90'];
+var list2 = [1, 23, 4]; //Array<元素类型>
+var list3 = ['1', '23', '4']; //Array<元素类型>
--- hello.ts
+++ hello.ts
@@ -1,6 +1,15 @@
-const hello = str => {
-    console.log('typescript');
-    return `hello,${str}`;
-}
+// const hello = str => {
+//     console.log('typescript');
+//     console.log('this is a function start');
+//     return `hello,${str}`;
+// }
 
-hello('daguo');
+// hello('daguo');
+
+let decLiteral:number = 6;      //数字类型
+let name1:string = 'bob';       //字符串类型
+let sentence:string = `hello,my name is ${name1}`;  //字符串模板
+let list0:number[] = [1,2,3,4];     //[]形式定义数组
+let list1:string[] = ['12','12','90'];
+let list2:Array<number> = [1,23,4]; //Array<元素类型>
+let list3:Array<string> = ['1','23','4'];   //Array<元素类型>
